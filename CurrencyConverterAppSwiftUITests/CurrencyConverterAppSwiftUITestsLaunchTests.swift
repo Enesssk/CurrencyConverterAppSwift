fbcb70cdc88f74a6dff4ad9096b39bfabe718427
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-03-03 13:02:16 +0300

    Initial Commit

---
+++ CurrencyConverterAppSwiftUITests/CurrencyConverterAppSwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CurrencyConverterAppSwiftUITestsLaunchTests.swift
+//  CurrencyConverterAppSwiftUITests
+//
+//  Created by Enes Kala on 3.03.2024.
+//
+
+import XCTest
+
+final class CurrencyConverterAppSwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
